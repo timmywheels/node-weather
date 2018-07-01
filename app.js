387d8d90315bdd1ba0d82716fc28e25c3337fa81
Tim Wheeler <tim@timwheeler.com>
2018-07-01 14:56:47 -0400

    created config.js

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const request = require('request');
+const config = require('./config.js');
 
 request({
-    url: 'https://maps.googleapis.com/maps/api/geocode/json?address=22%20oakhill%20avenue%20seekonk&key=AIzaSyCY6oMaNbK50IaV04fuKFwWDfeSm1L3kbc',
+    url: 'https://maps.googleapis.com/maps/api/geocode/json?address=' + config.ADDRESS + '&key=' + config.KEY,
     json: true
 }, (error, response, body) => {
     console.log(JSON.stringify(response, undefined, 2));
